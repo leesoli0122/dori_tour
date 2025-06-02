8e27e4d5fd4e5b144d0258f4c5ddcb18d2d32c8e
leesoli0122 <leesoli0122@gmail.com>
2025-06-02 11:00:46 +0900

    다시 수정

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  const text = "🎉 탑승자 임수인, 환영합니다! 🎉";
+  const line1 = "🎉 탑승자 이솔, 동천투어 탑승을 환영합니다. 🎉";
   const target = document.getElementById("typewriter");
   let index = 0;
 
   function type() {
-    if (index < text.length) {
-      target.innerHTML += text.charAt(index);
+    if (index < line1.length) {
+      target.innerHTML += line1.charAt(index);
       index++;
       setTimeout(type, 100);
+    } else {
+      // 다음 문장들 순차적으로 등장
+      setTimeout(() => {
+        document.getElementById("message1").classList.remove("hidden");
+      }, 1000);
+      setTimeout(() => {
+        document.getElementById("message2").classList.remove("hidden");
+        document.getElementById("plane").classList.remove("hidden");
+      }, 3000);
     }
   }
   type();
